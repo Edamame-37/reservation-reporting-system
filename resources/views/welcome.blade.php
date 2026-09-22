4c035b1256137809d980b56b0230312fef2a1fa3
Rafa Azlan <ra07zlan@gmail.com>
2026-09-22 18:57:49 +0700

    chore: remove ignored files from git tracking

--- resources/views/welcome.blade.php
+++
@@ -1,8 +0,0 @@
-{{-- 
-  NAMA FILE      : welcome.blade.php
-  FUNGSIONALITAS : Tampilan Beranda Default Root (/) CAVA
-  DESKRIPSI      : Menyajikan halaman beranda portal publik CAVA (Campus Venue Access) bagi pengunjung yang mengakses root domain.
-  CARA KERJA     : Memanggil view 'public.home' yang menggunakan master layout <x-public-layout>.
---}}
-
-@include('public.home')
