f106b805ece0520a49907a60af1c885d6300d3b1
Rafa Azlan <ra07zlan@gmail.com>
2026-09-24 11:54:59 +0700

    Update primary button color to slate-900

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex justify-center items-center px-6 py-3 bg-[#7C3AED] hover:bg-[#6D28D9] border border-transparent rounded-xl font-bold text-sm text-white uppercase tracking-wider focus:outline-none focus:ring-2 focus:ring-[#7C3AED] focus:ring-offset-2 dark:focus:ring-offset-gray-800 transform active:scale-95 transition-all ease-in-out duration-200 shadow-md hover:shadow-lg']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex justify-center items-center px-6 py-3 bg-slate-900 hover:bg-slate-800 border border-transparent rounded-xl font-bold text-sm text-white uppercase tracking-wider focus:outline-none focus:ring-2 focus:ring-slate-900 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transform active:scale-95 transition-all ease-in-out duration-200 shadow-md hover:shadow-lg']) }}>
     {{ $slot }}
 </button>
